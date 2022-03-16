064e722bb69437fad1a210c4d07ab58baa434a97
DevRUniversity <DevonRUniversity@protonmail.com>
2022-03-16 19:39:49 -0400

    Devon 3/16/2022
    -Fully implemented proposed code method to execute ground commands.

--- personnel_folders/drs_folder/nova_0920B_ro_main/SurfaceCommandsQueue.hh
+++ personnel_folders/drs_folder/nova_0920B_ro_main/SurfaceCommandsQueue.hh
@@ -16,35 +16,8 @@ class SurfaceCommandsQueue {
     }
 
     void accept_code_input(int code){
-        /*
-        Key to integer codes to trigger certain function calls:
-        (0) -> Do nothing
-        (1)  -> Test Transmission
-        (2)  -> Reserved for ground functions
-        (3)  -> Reserved for ground functions
-        (4)  -> Reserved for ground functions
-        (5)  -> Reserved for ground functions
-        (6)  -> Reserved for ground functions
-        (7)  -> Reserved for ground functions
-        (8)  -> Reserved for ground functions
-        (9)  -> Reserved for ground functions
-        (10) -> Order Tier 1 Data Packets
-        (11) -> Order Tier 2 Data Packets
-        */
-
-
-        //this may be quicker if done via accessing an array in memory where each code is an index and the array stores the address of the function.
-        //consider adding this to setup?
-        if(code == 0){
-           return;
-        }
-        surface_to_rocket_commands::acknowledge_command_recieved(); //Just tells the ground reciever to stop transmitting the command
-        if(code == 1){
-           functions_queue.add_to_queue(&surface_to_rocket_commands::test_transmission);
-        }
-        else{
-           //Send to log that an invalid code was recieved, log will be sent to ground
-        }
+        surface_to_rocket_commands::acknowledge_command_recieved(); //Just tells the ground reciever that the command was recieved.
+        functions_queue.add_to_queue(array_of_addresses_of_command_functions.at(code)); //Recall that code is the index of the relevant vector which stores the function address to execute it.
     }
     
  
